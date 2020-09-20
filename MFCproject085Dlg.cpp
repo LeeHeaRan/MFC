a9db9a9e6f535c75da66ad440abb6fa1e0479296
LHR <48555909+LeeHeaRan@users.noreply.github.com>
2020-09-21 02:21:25 +0900

    Add files via upload

---
+++ MFCproject085Dlg.cpp
@@ -0,0 +1,206 @@
+﻿
+// MFCproject085Dlg.cpp: 구현 파일
+//
+
+#include "stdafx.h"
+#include "MFCproject085.h"
+#include "MFCproject085Dlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+// 구현입니다.
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CMFCproject085Dlg 대화 상자
+
+
+
+CMFCproject085Dlg::CMFCproject085Dlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MFCPROJECT085_DIALOG, pParent)
+	, m_timeMC(COleDateTime::GetCurrentTime())
+	, m_timeTP(COleDateTime::GetCurrentTime())
+	, m_strDay(_T(""))
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFCproject085Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_MonthCalCtrl(pDX, IDC_MONTHCALENDAR1, m_timeMC);
+	DDX_DateTimeCtrl(pDX, IDC_DATETIMEPICKER1, m_timeTP);
+	DDX_Control(pDX, IDC_LIST1, m_listBox);
+	DDX_Text(pDX, IDC_EDIT1, m_strDay);
+}
+
+BEGIN_MESSAGE_MAP(CMFCproject085Dlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON2, &CMFCproject085Dlg::OnBnClickedButton2)
+	ON_BN_CLICKED(IDC_BUTTON1, &CMFCproject085Dlg::OnBnClickedButton1)
+END_MESSAGE_MAP()
+
+
+// CMFCproject085Dlg 메시지 처리기
+
+BOOL CMFCproject085Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+
+	// IDM_ABOUTBOX는 시스템 명령 범위에 있어야 합니다.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+void CMFCproject085Dlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 응용 프로그램의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CMFCproject085Dlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CMFCproject085Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CMFCproject085Dlg::OnBnClickedButton2()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	UpdateData();
+	int day = _ttoi(m_strDay);
+	//COleDateTimeSpan 일, 시간, 분, 초
+	m_timeMC = (m_timeTP - COleDateTimeSpan(day, 0, 0, 0));
+	UpdateData(false);
+}
+
+
+void CMFCproject085Dlg::OnBnClickedButton1()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	UpdateData();
+	CString str;
+	CString strEdit;
+	GetDlgItemText(IDC_EDIT2, strEdit);
+	//만약 edit control에 값이 있다면 str에 값을 추가하여 리스트 박스에 넣는다.
+	if (strEdit.IsEmpty() != true) {
+		str.Format(_T("%d-%d-%d: %s"), m_timeMC.GetYear(), m_timeMC.GetMonth(), m_timeMC.GetDay(), strEdit);
+		m_listBox.AddString(str);
+	}
+	//값이 없다면 날짜만 리스트 박스에 넣는다.
+	else {
+		str.Format(_T("%d-%d-%d"), m_timeMC.GetYear(), m_timeMC.GetMonth(), m_timeMC.GetDay());
+		m_listBox.AddString(str);
+	}
+	
+
+	//만약 에디트 박스에 값이 있다면 str에 값을 추가하여 리스트 박스에 추가한다.
+	UpdateData(false);
+}
+
+
+
+//사용 안함
+void CMFCproject085Dlg::OnBnClickedButton3()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+}
---
+++ MFCproject085Dlg.h
@@ -0,0 +1,42 @@
+﻿
+// MFCproject085Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMFCproject085Dlg 대화 상자
+class CMFCproject085Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMFCproject085Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCPROJECT085_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	COleDateTime m_timeMC; //캘린더 컨트롤 변수
+	COleDateTime m_timeTP; //데이터피커 변수
+	afx_msg void OnBnClickedButton2(); //입력 버튼
+	CListBox m_listBox; //리스트박스 변수
+	CString m_strDay; //에디트 컨트롤 변수
+	afx_msg void OnBnClickedButton1(); //리스트 추가 변수
+	afx_msg void OnBnClickedButton3(); //사용안함
+};
